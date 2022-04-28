7feb7f90719de04899ab770f15efb89b234ce637
Doshi <doshi@Doshis-MacBook-Pro.local>
2022-04-28 19:13:05 -0400

    top k freq elements complete

--- main.cpp
+++ main.cpp
@@ -18,6 +18,8 @@ int main(){
     Test({0, 1}, a->twoSum({2,7,11,15}, 9));
     Test({1, 2}, a->twoSum({3,2,4}, 6));
 
+    Test({1, 2}, a->topKFrequent({1,1,1,2,2,3},2));
+    Test({1}, a->topKFrequent({1}, 1));
 
     Test::printResult();
     return 0;
