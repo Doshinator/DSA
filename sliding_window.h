4fc32424491f3ed76bcaf2fcccf45e4aece680f9
Doshi <doshi@Doshis-MacBook-Pro.local>
2022-05-03 15:56:13 -0400

    new file added

---
+++ sliding_window.h
@@ -0,0 +1,14 @@
+#ifndef SLIDING_WINDOW_H_
+#define SLIDING_WINDOW_H_
+
+#include <vector>
+
+using namespace std;
+
+class SLIDING_WINDOW{
+    public:
+    int maxProfit(vector<int> prices);
+
+};
+
+#endif
