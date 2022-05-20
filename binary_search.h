e2ba26b8a7477cb5b3c63a4eb651eca233739a5e
Doshi <doshi@Doshis-MacBook-Pro.local>
2022-05-20 18:05:36 -0400

    test passes

--- binary_search.h
+++ binary_search.h
@@ -2,6 +2,8 @@
 #define BINARY_SEARCH_H_
 
 #include <vector>
+#include <string>
+#include <unordered_map>
 
 using namespace std;
 
@@ -11,6 +13,15 @@ class BINARY_SEARCH{
     bool searchMatrix(vector<vector<int>> matrix, int target);
     int minEatingSpeed(vector<int> piles, int h);
     int searchRotatedArray(vector<int> nums, int target);
+};
+
+class TimeMap{
+    private:
+    unordered_map<string, vector<pair<int,string>>> m;
+    public:
+    TimeMap();
+    void set(string key, string value, int timestamp);
+    string get(string key, int timestamp);
 };
 
 
