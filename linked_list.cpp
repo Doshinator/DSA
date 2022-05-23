54e27d273fb91b7f8b5f98d7ee5aab161f41b1c4
Doshi <doshi@Doshis-MacBook-Pro.local>
2022-05-23 17:23:33 -0400

    merged sorted list

--- linked_list.cpp
+++ linked_list.cpp
@@ -13,3 +13,26 @@ ListNode* LINKED_LIST::reverseList(ListNode* head){
     head = prev;
     return head;
 }
+
+
+ListNode* LINKED_LIST::mergeTwoLists(ListNode* list1, ListNode* list2){
+    if(!list1) return list2;
+    if(!list2) return list1;
+
+    ListNode* mergedList = new ListNode(0);
+    ListNode* node = mergedList;
+
+    while(list1 && list2){
+        if(list1->val < list2->val){
+            node->next = list1;
+            list1 = list1->next;
+        } else {
+            node->next = list2;
+            list2 = list2->next;
+        }
+        node = node->next;
+    }
+    
+    list1? node->next = list1 : node->next = list2;
+    return mergedList->next;
+}
