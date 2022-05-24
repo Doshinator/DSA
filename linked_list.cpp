db2ee57c6cbad11cb98850cf9ceb28cf4bb8ea42
Doshi <doshi@Doshis-MacBook-Pro.local>
2022-05-23 21:25:14 -0400

    test passes on lc

--- linked_list.cpp
+++ linked_list.cpp
@@ -30,9 +30,22 @@ void LINKED_LIST::reorderList(ListNode* head){
     // Output: [1,4,2,3]
     // l0 -> ln -> l1 -> ln-2 -> l2 -> ln-3...
 
-    ListNode* node = head;
-    ListNode* mid = LINKED_LIST::midNode(head);
+    ListNode* list1 = head;
 
+    ListNode* mid = LINKED_LIST::midNode(head);
     ListNode* list2 = reverseList(mid);
-    
+
+    int i = 1;
+    list1 = head->next;
+    while(list1 != list2){
+        if(i){
+            head->next = list2;
+            list2 = list2->next;
+        } else{
+            head->next = list1;
+            list1 = list1->next;
+        }
+            head = head->next;
+        i ^= 1;
+    }
 }
