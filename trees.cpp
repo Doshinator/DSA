928de82dece3a756b12e0a24eb8c1ef5ee1b7e44
Doshi <doshi@Doshis-MacBook-Pro.local>
2022-05-28 18:02:27 -0400

    invert tree test pass + struct added

--- trees.cpp
+++ trees.cpp
@@ -2,6 +2,16 @@
 
 
 TreeNode* TREES::invertTree(TreeNode* root){
+    if(root == nullptr)
+        return root;
 
-    return nullptr;
+    TreeNode* temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+    
+
+    invertTree(root->left);
+    invertTree(root->right);
+    
+    return root;
 }
