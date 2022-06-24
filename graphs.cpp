f4d04f2eef17f650fcc2506c3b4e7422b8414377
Doshi <doshi@Doshis-MacBook-Pro.local>
2022-06-24 00:43:19 -0400

    # islands passed

--- graphs.cpp
+++ graphs.cpp
@@ -5,22 +5,27 @@ using namespace std;
 
 
 int Graph::numIslands(vector<vector<char>>& grid){
-    /*
-    Input: grid = [
-        ["1","1","1","1","0"],
-        ["1","1","0","1","0"],
-        ["1","1","0","0","0"],
-        ["0","0","0","0","0"]
-        ]
-    Output: 1
+    int n_island = 0;
+    vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(), false));
+    for(int i = 0; i < grid.size(); i++){
+        for(int j = 0; j < grid[i].size(); j++){
+            if(!visited[i][j] && grid[i][j] == '1'){
+                numIslandsHelper(grid, visited, i, j);
+                n_island += 1;
+            }
+        }
+    }
+    return n_island;
+}
 
-    Input: grid = [
-        ["1","1","0","0","0"],
-        ["1","1","0","0","0"],
-        ["0","0","1","0","0"],
-        ["0","0","0","1","1"]
-        ]
-    Output: 3
-    */
 
+void Graph::numIslandsHelper(vector<vector<char>> &grid, vector<vector<bool>> &visited, int m, int n){
+    if(m < 0 || n < 0 || m > grid.size() - 1 || n > grid[0].size() - 1 || grid[m][n] == '0' || visited[m][n])
+        return;
+
+    visited[m][n] = true;   
+    numIslandsHelper(grid, visited, m + 1, n);
+    numIslandsHelper(grid, visited, m - 1, n);
+    numIslandsHelper(grid, visited, m, n + 1);
+    numIslandsHelper(grid, visited, m, n - 1);
 }
--- graphs.h
+++ graphs.h
@@ -5,5 +5,5 @@ using namespace std;
 class Graph{
     public:
     int numIslands(vector<vector<char>> &grid);
-    
+    void numIslandsHelper(vector<vector<char>> &grid, vector<vector<bool>> &visited, int m, int n);
 };
