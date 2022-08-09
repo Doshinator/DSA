7a417621cea38decd88fa64930ab5280176e1592
Doshi <doshi@Doshis-MacBook-Pro.local>
2022-08-09 00:54:47 -0400

    dp bottom-up

--- dp.cpp
+++ dp.cpp
@@ -12,6 +12,14 @@ int DP::climbStairs(int n){
 int DP::minCostClimbingStairs(vector<int> &cost){
     // Input: cost = [10,15,20]
     // Output: 15
-    
+    // int dp[cost.size() + 1];
+    // dp[0] = 0;
+    // dp[1] = 0;
+    vector<int> dp(cost.size() + 1);
+    for(int i = 2; i < cost.size(); i++){
+        dp[i] = min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2]);
+    }
+
+    return dp[cost.size()];
 }
 
