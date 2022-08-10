5454b7cef5908e21e300f026debe7df2659e98b4
Doshi <doshi@Doshis-MacBook-Pro.local>
2022-08-10 00:10:39 -0400

    tabulation method added

--- dp.h
+++ dp.h
@@ -7,7 +7,8 @@ class DP{
     int climbStairs(int n);
     int minCostClimbingStairs(vector<int> &cost);
     int rob(vector<int> &nums);
-    int rob(vector<int> &nums, vector<int> &dp, int i);
+    int robTabulation(vector<int> &nums);
+    int robMemo(vector<int> &nums, vector<int> &dp, int i);
 
     private:
     unordered_map<int, int> memoClimbStairs{{1, 1}, {2, 2}};
