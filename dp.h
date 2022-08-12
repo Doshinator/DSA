4db93751379bef84ed75ffa67eadd06ad28115ac
Doshi <doshi@Doshis-MacBook-Pro.local>
2022-08-11 23:03:11 -0400

    init

--- dp.h
+++ dp.h
@@ -6,9 +6,12 @@ class DP{
     public:
     int climbStairs(int n);
     int minCostClimbingStairs(vector<int> &cost);
+
     int rob(vector<int> &nums);
     int robTabulation(vector<int> &nums);
     int robMemo(vector<int> &nums, vector<int> &dp, int i);
+
+    int robII(vector<int> &nums);
 
     private:
     unordered_map<int, int> memoClimbStairs{{1, 1}, {2, 2}};
